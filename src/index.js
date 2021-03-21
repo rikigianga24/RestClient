05f451c9db69d8edaa82d23d2e8bf9335598d7cb
rikygianga24 <riccardo.giangani@hotmail.com>
2021-03-21 12:18:27 +0100

    creating structure

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
+
+//renderizza il contenuto del primo parametro su root
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+     <App />
+ ,
   document.getElementById('root')
 );
 
